78d325c97539ee43baff820f9332217accbaba18
idiotf <140891554+idiotf@users.noreply.github.com>
2023-09-02 17:24:02 +0900

    1.1.0 업데이트
    SUIT 폰트를 추가했습니다. 만약 다른 폰트를 원한다면 https://playentry.org/project/64f2e58a691dfb00253d81b3 의 댓글에 폰트 이름을 적어주세요.

--- nanumGothic.js
+++ nanumGothic.js
@@ -6,6 +6,16 @@ setInterval(function() {
 		for(const element of writing.querySelectorAll("[class*=se-ff-]")) {
 			//element.style.fontFamily = "se-nanumgothic,'나눔고딕',nanumgothic,sans-serif";
 			chrome.storage.local.get("font", value => {
+				if(value.font[0] === "_") {
+					if(!document.getElementById("suit-sheet")) {
+						const stylesheet = document.createElement("link");
+						stylesheet.href = "https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/static/woff2/SUIT.css";
+						stylesheet.rel = "stylesheet";
+						stylesheet.id = "suit-sheet";
+						document.head.appendChild(stylesheet);
+					}
+					element.style.fontFamily = "SUIT, sans-serif";
+				} else element.style.fontFamily = "";
 				element.className = element.className.replace(/se-ff-(.+)/s, "se-ff-" + (value.font || "nanumgothic"));
 			});
 		}
